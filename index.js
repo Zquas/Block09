3308a80dfb64eda0b080c8c54ba5312a3f899449
Jason Chen <114283388+Zquas@users.noreply.github.com>
2023-06-07 20:31:07 -0700

    Add files via upload
    added js

---
+++ index.js
@@ -0,0 +1,10 @@
+//create an alert message
+let message1 = "Jason's";
+let message2 = "website!";
+
+//create an const for the message.
+const message = `Welcome to ${message1} ${message2}`;
+
+//create the output for the message using both console.log and alert.
+alert(message);
+console.log(message);
