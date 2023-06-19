547876e5c4f1d9d0e62f3d58b03cf97f36c0722d
Jason Chen <114283388+Zquas@users.noreply.github.com>
2023-06-18 23:03:20 -0700

    Final update probably

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+firstEntry = 0;
+
 //create an alert message
 let message1 = "Jason's";
 let message2 = "website!";
@@ -6,5 +8,29 @@ let message2 = "website!";
 const message = `Welcome to ${message1} ${message2}`;
 
 //create the output for the message using both console.log and alert.
-alert(message);
-console.log(message);
+if (firstEntry != 0){
+    alert(message);
+    console.log(message);
+    firstEntry = 1;
+}
+
+const form = document.querySelector("#form")
+const submitButton = document.querySelector("#submit")
+const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+
+form.addEventListener('submit', e => {
+  submitButton.disabled = true
+  e.preventDefault()
+  let requestBody = new FormData(form)
+  fetch(scriptURL, { method: 'POST', body: requestBody})
+    .then(response => {
+       alert('Success!', response)
+       submitButton.disabled = false
+      })
+    .catch(error => {
+    alert('Error!', error.message)
+      submitButton.disabled = false
+
+    }
+    )
+})
